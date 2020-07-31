82cfef4d9df13e317a8fa6124862460540748a10
devansh-299 <f20180131@goa.bits-pilani.ac.in>
2020-07-31 18:40:36 +0530

    added mifos-rocketchat dependency and fixed build failures

--- mifospay/src/main/java/org/mifos/mobilewallet/mifospay/MifosPayApp.java
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/MifosPayApp.java
@@ -1,22 +1,29 @@
 package org.mifos.mobilewallet.mifospay;
 
+import android.app.Activity;
 import android.app.Application;
+import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.work.Worker;
 
 import com.mifos.mobile.passcode.utils.ForegroundChecker;
 
+import org.jetbrains.annotations.NotNull;
 import org.mifos.mobilewallet.mifospay.injection.component.ApplicationComponent;
 import org.mifos.mobilewallet.mifospay.injection.component.DaggerApplicationComponent;
 import org.mifos.mobilewallet.mifospay.injection.module.ApplicationModule;
 
 import butterknife.ButterKnife;
+import chat.rocket.android.app.RocketChatInjector;
+import dagger.android.AndroidInjector;
 
 /**
  * Created by naman on 17/8/17.
  */
 
-public class MifosPayApp extends Application {
+public class MifosPayApp extends Application implements RocketChatInjector {
 
     private static MifosPayApp instance;
 
@@ -58,6 +65,27 @@ public class MifosPayApp extends Application {
     // Needed to replace the component with a test specific one
     public void setComponent(ApplicationComponent applicationComponent) {
         this.applicationComponent = applicationComponent;
+    }
+
+    @NotNull
+    @Override
+    public AndroidInjector<Worker> workerInjector() {
+        return null;
+    }
+
+    @Override
+    public AndroidInjector<Activity> activityInjector() {
+        return null;
+    }
+
+    @Override
+    public AndroidInjector<BroadcastReceiver> broadcastReceiverInjector() {
+        return null;
+    }
+
+    @Override
+    public AndroidInjector<Service> serviceInjector() {
+        return null;
     }
 }
 
